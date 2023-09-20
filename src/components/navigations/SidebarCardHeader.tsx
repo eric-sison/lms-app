75eb7cab09309910a2434584c8e8097b57b00019
Eric Sison <ericsison.dev@gmail.com>
2023-09-20 09:10:43 +0800

    added sidebar component for user navigation

--- src/components/navigations/SidebarCardHeader.tsx
+++ src/components/navigations/SidebarCardHeader.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from "react";
 
 export const SidebarCardHeader: FunctionComponent = () => {
   return (
-    <div className="mb-7 px-1 flex items-center gap-3">
+    <div className="mb-10 px-1 flex items-center gap-3">
       <Image
         src="/mock/profile5.jpg"
         alt="profile"
@@ -12,9 +12,9 @@ export const SidebarCardHeader: FunctionComponent = () => {
         className="inline-block h-12 w-12 rounded-full object-cover shrink-0 border-2"
       />
 
-      <section className="leading-5">
-        <h3 className="text-zinc-200 font-medium">Eric C. Sison</h3>
-        <p className="text-zinc-400 text-sm">ericsison@gscwd.com</p>
+      <section className="leading-5 w-44">
+        <h3 className="text-zinc-200 font-medium">John S. Doe</h3>
+        <p className="text-zinc-400 text-sm truncate">john.doe@gscwd.com</p>
       </section>
     </div>
   );
